23861f63ba85ed53f411d4c8d34f892a824e7275
massaro8 <massaro.donato@virgilio.it>
2020-11-26 15:16:21 +0100

    pp

--- app/src/main/java/com/example/performance2/registrati.java
+++ app/src/main/java/com/example/performance2/registrati.java
@@ -2,13 +2,28 @@ package com.example.performance2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registrati extends AppCompatActivity {
+    protected Button tlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrati);
+        tlogin = (Button) findViewById(R.id.tlogin);
+                tlogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openAcitivtytlogin();
+                    }
+                });
+    }
+    public void openAcitivtytlogin(){
+        Intent intent = new Intent(this, Login.class);
+                startActivity(intent);
     }
 }
