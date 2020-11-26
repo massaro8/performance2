75cfdec08632ba84642c6ac4a4b386a2dce24934
massaro8 <massaro.donato@virgilio.it>
2020-11-26 18:32:22 +0100

    pp

--- app/src/main/java/com/example/performance2/MainActivity.java
+++ app/src/main/java/com/example/performance2/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.performance2;
 
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+
+
     }
 }
