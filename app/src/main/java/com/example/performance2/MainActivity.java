0e8267eaa423968f791e4d2a84761e5b1e4024e7
massaro8 <massaro.donato@virgilio.it>
2020-12-03 17:30:29 +0100

    pp

--- app/src/main/java/com/example/performance2/MainActivity.java
+++ app/src/main/java/com/example/performance2/MainActivity.java
@@ -10,16 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+     NavigationView navigationView;
+    FragmentManager fragmentManager;
+   FragmentTransaction fragmentTransaction;
+   ActionBarDrawerToggle actionBarDrawerToggle;
 
 
 
@@ -29,24 +35,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.openDrawer,
-                R.string.closeDrawer
-        );
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle  = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+
+
+        // carica il fragmwnt predefinito
+        fragmentManager=getSupportFragmentManager();
+        fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment,new MainFragment());
+        fragmentTransaction.commit();
+
+
     }
 
 
@@ -69,13 +78,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if(menuitem.getItemId() == R.id.home){
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new MainFragment());
+            fragmentTransaction.commit();
+
+
+        }
+        if (menuitem.getItemId() == R.id.note){
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new fragment_note ());
+            fragmentTransaction.commit();
+
+        }
+        if (menuitem.getItemId() == R.id.news){
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new fragment_news());
+            fragmentTransaction.commit();
+
+        }
+        if (menuitem.getItemId() == R.id.to_do_list){
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new fragment_todolist ());
+            fragmentTransaction.commit();
+
+        }
+        if (menuitem.getItemId() == R.id.upload){
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new fragment_upload ());
+            fragmentTransaction.commit();
+
+        }
+        if (menuitem.getItemId() == R.id.calendario){
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new fragment_calendario ());
+            fragmentTransaction.commit();
+
+        }
+       
         return false;
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
 
 
-    }
 }
